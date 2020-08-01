44111738feff434fb78b23c6a4781cc3bbf83553
ooihuiying <huang.ooihuiying@gmail.com>
2020-08-01 15:56:18 +0800

    Connects to MongoDB Atlas and complete API call for getting all results

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const VenueRoute = require("./VenueRoute")
 
-router.get('/', function (req, res, next) {
-    res.send('Test')
-})
-
+router.get('/venues', VenueRoute.getAll);
 module.exports = router;
