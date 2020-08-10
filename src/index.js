a1e1054e863f89002f400faa31fe303b233f719f
ooiy0006 <ooiy0006@e.ntu.edu.sg>
2020-08-10 21:14:32 +0800

    allow CORS

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const bodyParser = require('body-parser');
 const swagger = require('swagger-ui-express');
 const swaggerDoc = require('../swagger');
 const router = require('./routes/index.js');
+var cors = require('cors');
 
 function setUpRoutes(app) {
   console.log('Initialisation has been completed... ');
   app.use(bodyParser.json());
+  app.use(cors({ credentials: true, origin: true }));
   app.use('/api-docs', swagger.serve, swagger.setup(swaggerDoc));
   app.use('/api/0.1', router);
 }
