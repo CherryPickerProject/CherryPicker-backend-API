9c8c3de37589466daabc4a4e6a8f998de2d1de73
ooihuiying <huang.ooihuiying@gmail.com>
2020-08-16 21:50:31 +0800

    Fix regex issue for query

--- src/models/VenueModel.js
+++ src/models/VenueModel.js
@@ -3,18 +3,18 @@ const MongoDB = require('../libs/MongoDB_Atlas');
 // TODO: Currently only filering results based on tags and pax
 // More filtering will be done on elasticsearch side
 async function getAll(query) {
-  const filteredQuery = {
-    tags: { $regex: query.keyword ? query.keyword : {}, $options: 'i' },
-    pax: { $gte: query.pax ? parseInt(query.pax) : 0 }
-  };
-  return await MongoDB.getAllVenues(filteredQuery, query.activePage);
+    const filteredQuery = {
+        tags: { $regex: query.keyword ? query.keyword : '', $options: 'i' },
+        pax: { $gte: query.pax ? parseInt(query.pax) : 0 }
+    };
+    return await MongoDB.getAllVenues(filteredQuery, query.activePage);
 }
 
 async function getOne(venueId) {
-  return await MongoDB.getOneVenue(venueId);
+    return await MongoDB.getOneVenue(venueId);
 }
 
 module.exports = {
-  getAll,
-  getOne
+    getAll,
+    getOne
 };
